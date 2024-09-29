3b3f77b885e89e726f0f6c827b879018e0d6e07b
Farhan Tamim <farhantamim@Farhans-MacBook-Pro.local>
2024-09-29 21:16:04 +0600

    second commit

---
+++ Array introduction/Array.java
@@ -0,0 +1,10 @@
+public class Array {
+    public static void main(String[] args) {
+        int[] marks = new int[3];
+        marks[0] = 97;
+        marks[1] = 98;
+        marks[2] = 95;
+
+        System.out.print(marks[1]);
+    }
+}
--- Loops in Java/Solutions.java
+++ Loops in Java/Solutions.java
@@ -246,3 +246,89 @@
 //        }
 //     }
 // }
+// ............................................................................................
+// Two numbers are entered by the user, x and n. Write a function to find the value of one 
+// number raised to the power of another i.e. xn.
+// import java.util.*;
+// public class Loops {
+//    public static void main(String args[]) {
+//        System.out.print("Enter x : ");
+//        Scanner sc = new Scanner(System.in);
+//        int x = sc.nextInt();
+//        System.out.print("Enter n : ");
+//        int n = sc.nextInt();
+
+//        int result = 1;
+//        //Please see that n is not too large or else result will exceed the size of int
+//        for(int i=0; i<n; i++) {
+//            result = result * x;
+//        }
+
+//        System.out.println("x to the power n is : "+ result);
+
+//        sc.close();
+//    }   
+// }
+// ............................................................................................
+// Two numbers are given x and n. Write a programme to find the value of one 
+// number raised to the power of another i.e. xn.
+// public class Loops{
+//     public static void main(String[] args) {
+//         int n=4;
+//         int x=6;
+//         int result=1;
+        
+//         for(int i=0; i<n; i++) {
+//             result=result*x;
+//         }
+//         System.out.print(result);
+//     }
+// }
+// output:
+// 1296
+// ............................................................................................
+// Write an infinite loop using do while condition.
+// import java.util.*;
+// public class Loops {
+//    public static void main(String args[]) {
+//       do {
+
+
+//       } while(true);
+//    }   
+// }
+// ............................................................................................
+// Write a program to enter the numbers till the user wants and at the end it should display the 
+// count of positive, negative and zeros entered. 
+// import java.util.*;
+// public class Solutions {
+//    public static void main(String args[]) {
+//        int positive = 0, negative = 0, zeros = 0;
+//        System.out.println("Press 1 to continue & 0 to stop");
+//        Scanner sc = new Scanner(System.in);
+//        int input = sc.nextInt();
+
+//        while(input == 1) {
+//            System.out.println("Enter your number : ");
+//            int number = sc.nextInt();
+//            if(number > 0) {
+//                positive++;
+//            } else if(number < 0) {
+//                negative++;
+//            } else {
+//                zeros++;
+//            }
+
+//            System.out.println("Press 1 to continue & 0 to stop");
+//            input = sc.nextInt();
+//        }
+
+//        System.out.println("Positives : "+ positive);
+//        System.out.println("Negatives : "+ negative);
+//        System.out.println("Zeros : "+ zeros);
+
+//        sc.close();
+//    }   
+// }
+// ............................................................................................
+
