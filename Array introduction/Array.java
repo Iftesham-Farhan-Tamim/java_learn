a9594440fd3875e73afac19189c393aecc0aec75
Farhan Tamim <farhantamim@Farhans-MacBook-Pro.local>
2024-09-30 00:31:16 +0600

    second commit

--- Array introduction/Array.java
+++ Array introduction/Array.java
@@ -52,24 +52,24 @@
 // 98
 // ............................................................................................
 // Taking an array as an input and printing its elements.
-// import java.util.*;
-// public class Main{
-//     public static void main(String[] args) {
-//         Scanner sc = new Scanner(System.in);
+import java.util.*;
+public class Array{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         
-//         int size = sc.nextInt();
-//         int[] arr = new int[size];
+        int size = sc.nextInt();
+        int[] arr = new int[size];
         
-//         for(int i=0; i<size; i++) {
-//             arr[i] = sc.nextInt();
-//         }
-//         for(int i=0; i<arr.length; i++) {
-//             System.out.print(arr[i] + " ");
-//         }
+        for(int i=0; i<size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for(int i=0; i<arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
 
-//         sc.close();
-//     }
-// }
+        sc.close();
+    }
+}
 
 // ............................................................................................
 
