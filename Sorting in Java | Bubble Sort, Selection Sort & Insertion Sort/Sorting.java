4a0df19cc9a339b171fcc53b661fe13212391e58
Farhan Tamim <farhantamim@Farhans-MacBook-Pro.local>
2024-10-06 12:17:44 +0600

    second commit

--- Sorting in Java | Bubble Sort, Selection Sort & Insertion Sort/Sorting.java
+++ Sorting in Java | Bubble Sort, Selection Sort & Insertion Sort/Sorting.java
@@ -33,6 +33,31 @@
 // Selection Sort
 // Idea: The inner loop selects the minimum element in the unsorted array 
 // and places the elements in increasing order. 
-// Time complexity: O(N2) 
+// Time complexity: O(N^2) 
 
+// public class Main{
+//     public static void main(String[] args) {
+//         int arr[] = {7, 8, 1, 3, 2};
+//         for(int i=0; i<arr.length-1; i++) {
+//           int smallest = i;
+//           for(int j=i+1; j<arr.length; j++) {
+//               if(arr[j] < arr[smallest]) {
+//                   smallest = j;
+//               }
+//           }
+        
+//           //swap
+//           int temp = arr[smallest];
+//           arr[smallest] = arr[i];
+//           arr[i] = temp;
+//       }
+       
+       
+//       for(int i=0; i<arr.length; i++) {
+//           System.out.print(arr[i]+" ");
+//       }
+       
+//       System.out.println();
+//     }
+// }
 // ............................................................................................
