823c75d4d76188c8b5deb26f84babcf80dde3185
Farhan Tamim <farhantamim@Farhans-MacBook-Pro.local>
2024-10-06 23:08:14 +0600

    second commit

--- Recursion 1/Recursion.java
+++ Recursion 1/Recursion.java
@@ -108,25 +108,25 @@
 // 3
 // ............................................................................................
 // Print x^n (with stack height = n)
-public class Recursion {
-    public static int printPower(int x, int n) {
-        if(n == 0) {
-            return 1;
-        }
-        if(x == 0) {
-            return 0;
-        }
+// public class Recursion {
+//     public static int printPower(int x, int n) {
+//         if(n == 0) {
+//             return 1;
+//         }
+//         if(x == 0) {
+//             return 0;
+//         }
     
-        int x_ = printPower(x, n-1);
-        int xn = x * x_;
-        return xn;
-    }
-    public static void main(String args[]) {
-        int x = 2, n = 5;
-        int output = printPower(x, n);
-        System.out.println(output);
-    }
-}
+//         int x_ = printPower(x, n-1);
+//         int xn = x * x_;
+//         return xn;
+//     }
+//     public static void main(String args[]) {
+//         int x = 2, n = 5;
+//         int output = printPower(x, n);
+//         System.out.println(output);
+//     }
+// }
 
 // output:
 // 32
